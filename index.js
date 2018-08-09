1594e45404d8873073e0c8f07a2f353124c4c4d0
Malik Glossop <malik.glossop@gmail.com>
2018-08-09 02:11:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians,instruments) {
   var output = []
   for(var i = 0; i < musicians.length; ++i) {
-    output.push(`${musicians[i]} plays ${instrucments[i]}`)
+    output.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return output
 }
