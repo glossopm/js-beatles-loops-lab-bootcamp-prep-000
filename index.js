438b87d660ec8fcf6bc0200e41cbe2e07f446db6
Malik Glossop <malik.glossop@gmail.com>
2018-08-09 02:10:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments) {
+  var output = []
+  for(var i = 0; i < musicians.length, ++i) {
+    output.push(`${musicians[i]} plays ${instrucments[i]}`)
+  }
+  return output
+}
